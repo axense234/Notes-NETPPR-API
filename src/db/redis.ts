5fe16eb97ff40a91d86364924af54476954e20d6
axense234 <andreicomanescuonline@gmail.com>
2023-01-08 16:53:57 +0200

    Added an url to redisClient after creating a redis instance on render.

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: "redis://red-cetdhco2i3mj6pi0ra20:6379",
+});
 
 const connectToRedis = async () => {
   await redisClient.connect();
